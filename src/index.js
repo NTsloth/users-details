97d64e7159faf10f072012d4d66317027a26911e
NTsloth <sloth2003nt@gmail.com>
2023-10-04 23:34:36 +0400

    user details

--- src/index.js
+++ src/index.js
@@ -1,7 +1,24 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
 import App from "./App";
+import userReducer from "./helpers/UserSlice";
+import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const store = configureStore({
+  reducer: {
+    user: userReducer,
+  },
+});
+
+const rootElement = document.getElementById("root");
+
+ReactDOM.render(
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  rootElement
+);
