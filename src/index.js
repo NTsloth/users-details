9f5c31be9630cbe830db9fdc46c92e9736080319
NTsloth <sloth2003nt@gmail.com>
2023-10-04 23:29:24 +0400

    user details

--- src/index.js
+++ src/index.js
@@ -1,25 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { Provider } from "react-redux";
-import { configureStore } from "@reduxjs/toolkit";
-import App from "./App";
-import userReducer from "../src/helpers/UserSlice";
+import ReactDOM from "react-dom/client";
 import "./index.css";
+import App from "./App";
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
-
-const root = document.getElementById("root");
-
-const reactRoot = ReactDOM.createRoot(root);
-
-reactRoot.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
-);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
